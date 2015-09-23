cffdb96201007b2f39820b9f93702e53b828917b
Phillip Webb <pwebb@pivotal.io>
2015-09-23 12:51:17 -0700

    Start to sketch out MSMT

--- src/main/java/io/pivotal/boothacks/MessageSourceMessageTransformer.java
+++ src/main/java/io/pivotal/boothacks/MessageSourceMessageTransformer.java
@@ -2,16 +2,24 @@ package io.pivotal.boothacks;
 
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageSourceMessageTransformer implements MessageTransformer {
 
+	private final MessageSource messageSource;
+
+	@Autowired
+	public MessageSourceMessageTransformer(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	@Override
 	public String transform(String message, Locale locale) {
-		// TODO Auto-generated method stub
+		// FIXME call messageSource.getMessage(code, args, locale)
 		return null;
 	}
 
 }
-
