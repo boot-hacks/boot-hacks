fe31920b96523e65c482327db644fe46366bd967
Phillip Webb <pwebb@pivotal.io>
2015-09-23 12:50:59 -0700

    Move old transformer to @Profile

--- src/main/java/io/pivotal/boothacks/HelloMessageTransfomer.java
+++ src/main/java/io/pivotal/boothacks/HelloMessageTransfomer.java
@@ -3,9 +3,11 @@ package io.pivotal.boothacks;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("legacy")
 public class HelloMessageTransfomer implements MessageTransformer {
 
 	private BootHacksProperties properties;
