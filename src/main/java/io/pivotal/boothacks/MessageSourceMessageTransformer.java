1bbbc6b1dc0b687808d5d6bc20cd808d8e53c906
Phillip Webb <pwebb@pivotal.io>
2015-09-23 16:50:12 -0700

    Add MessageSourceMessageTransformer

---
+++ src/main/java/io/pivotal/boothacks/MessageSourceMessageTransformer.java
@@ -0,0 +1,17 @@
+package io.pivotal.boothacks;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageSourceMessageTransformer implements MessageTransformer {
+
+	@Override
+	public String transform(String message, Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
