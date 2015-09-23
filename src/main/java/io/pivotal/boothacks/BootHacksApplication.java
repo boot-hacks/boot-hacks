6dbdd817d4e55c0f0059efe732b8e01a2d54ae20
Phillip Webb <pwebb@pivotal.io>
2015-09-23 13:14:00 -0700

    Add MessageSource bean

--- src/main/java/io/pivotal/boothacks/BootHacksApplication.java
+++ src/main/java/io/pivotal/boothacks/BootHacksApplication.java
@@ -3,10 +3,20 @@ package io.pivotal.boothacks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 @EnableConfigurationProperties(BootHacksProperties.class)
 public class BootHacksApplication {
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("mymessages");
+		return messageSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootHacksApplication.class, args);
